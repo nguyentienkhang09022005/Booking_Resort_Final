b5fbe9a3527a184ccec0a7fa1770dd5c70dee307
TienKhang <nguyentienkhang09022005@gmail.com>
2025-05-08 18:43:10 +0700

    update

---
+++ Booking_Resort/Booking_Resort/src/main/java/com/example/Booking_Resort/config/DotenvConfig.java
@@ -0,0 +1,20 @@
+package com.example.Booking_Resort.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DotenvConfig {
+
+    @PostConstruct
+    public void loadEnv() {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
+}
