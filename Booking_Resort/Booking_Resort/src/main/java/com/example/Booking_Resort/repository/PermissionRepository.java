a6159497d76f5e754eb5c4a98002e31ede774ad9
DauXu <nguyentienkhang09022005@gmail.com>
2025-05-14 17:33:44 +0700

    Delete Booking_Resort directory

--- Booking_Resort/Booking_Resort/src/main/java/com/example/Booking_Resort/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Booking_Resort.repository;
-
-import com.example.Booking_Resort.models.Permissions;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface PermissionRepository extends JpaRepository<Permissions, String> { }
--- Booking_Resort/Booking_Resort/src/main/java/com/example/Booking_Resort/repository/ServiceRSRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Booking_Resort.repository;
-
-import com.example.Booking_Resort.models.ServiceRS;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRSRepository extends JpaRepository<ServiceRS, String> {
-}
