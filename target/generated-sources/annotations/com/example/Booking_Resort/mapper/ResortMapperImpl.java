77f127bf312daa08092b67065fcc7692c2666257
TienKhang <nguyentienkhang09022005@gmail.com>
2025-06-07 02:44:42 +0700

    update report

--- target/generated-sources/annotations/com/example/Booking_Resort/mapper/ResortMapperImpl.java
+++ target/generated-sources/annotations/com/example/Booking_Resort/mapper/ResortMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-07T02:12:11+0700",
+    date = "2025-06-07T02:42:42+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.14 (Oracle Corporation)"
 )
 @Component
--- target/generated-sources/annotations/com/example/Booking_Resort/mapper/RoomMapperImpl.java
+++ target/generated-sources/annotations/com/example/Booking_Resort/mapper/RoomMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-28T02:16:26+0700",
+    date = "2025-06-07T02:42:42+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.14 (Oracle Corporation)"
 )
 @Component
